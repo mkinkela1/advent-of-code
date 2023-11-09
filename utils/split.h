a239e863cea2d6d1168c1f8b0470fb2827a910ac
Matteo Kinkela <mkinkela123@gmail.com>
2023-11-09 17:49:35 +0100

    [2016] days 1 and 2

---
+++ utils/split.h
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+auto split(string s, string delimiter) -> vector<string>
+{
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    string token;
+    vector<string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != string::npos)
+    {
+        token = s.substr(pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back(token);
+    }
+
+    res.push_back(s.substr(pos_start));
+    return res;
+}
