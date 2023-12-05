ae12a1cab950bf3f3773f0308877f34acaa8a6bf
Matteo <mkinkela123@gmail.com>
2023-12-05 18:25:59 +0100

    [2023] part 1, part 2 wip

---
+++ 2023/day-5/solution.cpp
@@ -0,0 +1,207 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <fstream>
+#include <tuple>
+#include <map>
+#include <queue>
+#include <stack>
+#include <array>
+#include <unordered_map>
+#include "../../utils/split.h"
+
+using namespace std;
+
+enum Mapper
+{
+    SEED_TO_SOIL,
+    SOIL_TO_FERTILIZER,
+    FERTILIZER_TO_WATER,
+    WATER_TO_LIGHT,
+    LIGHT_TO_TEMPERATURE,
+    TEMPERATURE_TO_HUMIDITY,
+    HUMIDITY_TO_LOCATION
+};
+
+struct Range
+{
+    long long destRangeStart, sourceRangeStart, rangeLength;
+    Range() {}
+    Range(long long destRangeStart, long long sourceRangeStart, long long rangeLength)
+    {
+        this->destRangeStart = destRangeStart;
+        this->sourceRangeStart = sourceRangeStart;
+        this->rangeLength = rangeLength;
+    }
+};
+
+class Solution
+{
+private:
+    fstream fin;
+    vector<long long> seeds;
+    vector<vector<Range>> ranges;
+
+    Mapper stringToEnum(string mapper)
+    {
+        if (mapper == "seed-to-soil")
+            return SEED_TO_SOIL;
+        else if (mapper == "soil-to-fertilizer")
+            return SOIL_TO_FERTILIZER;
+        else if (mapper == "fertilizer-to-water")
+            return FERTILIZER_TO_WATER;
+        else if (mapper == "water-to-light")
+            return WATER_TO_LIGHT;
+        else if (mapper == "light-to-temperature")
+            return LIGHT_TO_TEMPERATURE;
+        else if (mapper == "temperature-to-humidity")
+            return TEMPERATURE_TO_HUMIDITY;
+        else if (mapper == "humidity-to-location")
+            return HUMIDITY_TO_LOCATION;
+
+        throw "Invalid mapper";
+    }
+
+    void parse()
+    {
+        string line;
+        getline(this->fin, line);
+
+        vector<string> seed_input = split(line, ": ");
+        vector<string> seeds = split(seed_input[1], " ");
+
+        for (auto seed : seeds)
+        {
+            if (seed != "")
+                this->seeds.push_back(stoll(seed));
+        }
+
+        getline(this->fin, line);
+
+        for (int i = 0; i < 7; i++)
+        {
+            vector<Range> range;
+
+            getline(this->fin, line);
+
+            while (true)
+            {
+                getline(this->fin, line);
+                if (line == "")
+                {
+                    this->ranges.push_back(range);
+                    range.clear();
+                    break;
+                }
+                long long destRangeStart, sourceRangeStart, rangeLength;
+                sscanf(line.c_str(), "%lld %lld %lld", &destRangeStart, &sourceRangeStart, &rangeLength);
+
+                range.push_back(Range(destRangeStart, sourceRangeStart, rangeLength));
+            }
+        }
+    }
+
+    auto checkIntervals(vector<vector<Range>> &ranges, vector<pair<long long, long long>> seeds, long long target) -> long long
+    {
+        for (auto &mapper : this->ranges)
+        {
+            bool found = false;
+            for (auto &range : mapper)
+            {
+                if (target >= range.destRangeStart && target < range.destRangeStart + range.rangeLength)
+                {
+                    target = range.sourceRangeStart + (target - range.destRangeStart);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+                return -1;
+        }
+
+        for (auto seed : seeds)
+        {
+            if (target >= seed.first && target < seed.second)
+                return target;
+        }
+
+        return -1;
+    }
+
+public:
+    Solution(string fileName)
+    {
+        this->fin.open(fileName, ios::in);
+        this->parse();
+        this->fin.close();
+    }
+
+    auto part1() -> long long
+    {
+        for (auto mapper : this->ranges)
+        {
+            for (auto &seed : this->seeds)
+            {
+                for (auto range : mapper)
+                {
+                    if (seed >= range.sourceRangeStart && seed < range.sourceRangeStart + range.rangeLength)
+                    {
+                        seed = range.destRangeStart + (seed - range.sourceRangeStart);
+                        break;
+                    }
+                }
+            }
+        }
+
+        long long sol = __LONG_LONG_MAX__;
+        for (auto seed : this->seeds)
+            sol = min(sol, seed);
+
+        return sol;
+    }
+    auto part2() -> long long
+    {
+        vector<pair<long long, long long>> seeds;
+
+        for (int i = 0; i < this->seeds.size(); i += 2)
+        {
+            seeds.push_back(make_pair(this->seeds[i], this->seeds[i + 1]));
+        }
+
+        vector<Range> locationMappers = this->ranges[6];
+        this->ranges.pop_back();
+
+        reverse(this->ranges.begin(), this->ranges.end());
+        sort(locationMappers.begin(), locationMappers.end(), [](Range a, Range b)
+             { return a.destRangeStart < b.destRangeStart; });
+
+        for (auto &locationMapper : locationMappers)
+        {
+            for (long long dest = locationMapper.destRangeStart; dest < locationMapper.destRangeStart + locationMapper.rangeLength; dest++)
+            {
+                cout << dest << endl;
+                if (this->checkIntervals(this->ranges, seeds, dest) != -1)
+                    return dest;
+            }
+        }
+
+        return -1;
+    }
+};
+
+auto main() -> int
+{
+
+    Solution aoc = Solution("input.txt");
+    Solution test1 = Solution("test1.txt");
+    Solution test2 = Solution("test2.txt");
+
+    cout << "Part 1 test: " << test1.part1() << endl;
+    cout << "Part 1: " << aoc.part1() << endl;
+    cout << "--------------------------" << endl;
+    cout << "Part 2 test: " << test2.part2() << endl;
+    cout << "Part 2: " << aoc.part2() << endl;
+
+    return 0;
+}
